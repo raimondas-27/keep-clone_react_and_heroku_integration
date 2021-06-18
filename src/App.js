4173ddc0ab969a1d01197936a6c82408f746992e
raimondas-27 <raimondasvilnius@yahoo.com>
2021-06-18 15:15:15 +0300

    take notes sending

--- src/App.js
+++ src/App.js
@@ -6,18 +6,33 @@ import AppNavbar from "./components/appNavbar/appNavbar";
 import "./App.css";
 
 class App extends Component {
-  state = {}
+   state = {
+      notes: [],
+      noteId : 1,
+   };
 
-  render() {
-    return (
-        <div className="App">
-          <AppNavbar/>
-          <AppAside/>
-          <AppAddNote/>
-          <AppNoteList/>
-        </div>
-    );
-  }
+   handleNewNote = (newNote) => {
+      console.log('adding new note');
+      const newNoteOb = this.createNewNote(newNote);
+      console.log(newNoteOb);
+      const notesCopy = [...this.state.notes, newNoteOb]
+      this.setState({notes: notesCopy, noteId: this.state.noteId + 1})
+   };
+
+   createNewNote = (newNote) => {
+      return {_id: this.state.noteId, ...newNote, isPinned:false}
+   }
+
+   render() {
+      return (
+          <div className="App">
+             <AppNavbar/>
+             <AppAside/>
+             <AppAddNote onNewNote={this.handleNewNote} />
+             <AppNoteList/>
+          </div>
+      );
+   }
 }
 
 export default App;
