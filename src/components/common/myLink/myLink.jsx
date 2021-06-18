2a5e737a8284e0862e0bfd5c444d58d34dec8817
raimondas-27 <raimondasvilnius@yahoo.com>
2021-06-18 10:06:13 +0300

    myLink files sceleton, css for myLink added

---
+++ src/components/common/myLink/myLink.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class MyLink extends Component {
+   state = {}
+
+   render() {
+      return (
+          <div>
+             icon pavadinimas
+          </div>
+      );
+   }
+}
+
+export default MyLink;
